fbfb90412253494ab0f25dba0aef711c6a2b39de
Franck de Goër <fdg@udtq.fr>
2015-01-16 10:43:17 +0100

    First attempt to extend note segment size

--- src/elf64_patch.c
+++ src/elf64_patch.c
@@ -36,15 +36,46 @@ void infect(ELF *bin, unsigned char *payload, Elf64_Off offset, Elf64_Half pl_si
 	return;
 }
 
+
+void offset_segments(ELF *bin, Elf64_Off pnote_off, Elf64_Xword p_off) {
+    Elf64_Phdr *phr;
+    size_t i;
+    for (i = 0; i < bin->ehr->e_phnum; i++) {
+        phr = bin->phr[i];
+        if (phr->p_offset > pnote_off)
+            phr->p_offset += p_off;
+
+    }
+    return;
+}
+
+
+void offset_sections(ELF *bin, Elf64_Off pnote_off, Elf64_Xword p_off) {
+    Elf64_Shdr *shr;
+    size_t i;
+    for (i = 0; i < bin->ehr->e_shnum; i++) {
+        shr = bin->shr[i];
+        if (shr->sh_offset > pnote_off)
+            shr->sh_offset += p_off;
+        else if (shr->sh_offset + shr->sh_size >= pnote_off)
+            shr->sh_size += p_off;
+
+    }
+    return;
+}
+
+
 void Elf64_patch_note(ELF *bin, unsigned char *pl, Elf64_Half *pl_size, char *out_fname) {
 	// TODO if no pnote (ie NULL)
 	Elf64_Phdr *pnote = Elf64_get_pnote(bin);
 	if (*pl_size > pnote->p_filesz) {
-		printf("ERROR: payload is too large to fit in Note section \
-		(not implemented yet)\n");
-		exit(EXIT_NOT_IMPLEMENTED);
-	}
-	pl = pad_payload(pl, pl_size, pnote->p_filesz, 0x00);
+        offset_segments(bin, pnote->p_offset, *pl_size - pnote->p_filesz);
+        offset_sections(bin, pnote->p_offset, *pl_size - pnote->p_filesz);
+        pnote->p_filesz = *pl_size;
+        pnote->p_memsz = *pl_size;
+	} else { 
+    	pl = pad_payload(pl, pl_size, pnote->p_filesz, 0x00);
+    }    
 	// TODO if no pcode (ie NULL)
 	Elf64_Phdr *pcode = Elf64_get_pcode(bin);
 	pnote->p_type = PT_LOAD;
